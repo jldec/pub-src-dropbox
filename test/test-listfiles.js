eb56a6a75239ca038db482b8977816cc76b22a98
jldec <jurgen@ciaosoft.com>
2022-01-23 11:18:18 -0500

    v2.0.12 update deps

--- test/test-listfiles.js
+++ test/test-listfiles.js
@@ -1,6 +1,6 @@
 /**
  * pub-src-dropbox test-listfiles
- * copyright 2015-2020, Jürgen Leschner - github.com/jldec - MIT license
+ * Copyright (c) 2015-2022 Jürgen Leschner - github.com/jldec - MIT license
  *
 **/
 
